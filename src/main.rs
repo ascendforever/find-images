06f49f69e41734b8505b2887edfbd03d1eb8bf68
ascendforever <georgeh124v2@gmail.com>
2026-02-13 15:50:33 -0500

    Switch to `clap`; add flags --verbose and --quiet; clean up; and raise version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-extern crate chrono;
-extern crate shlex;
-extern crate structopt;
-use crate::structopt::StructOpt;
+use clap::Parser;
 use std::collections::HashSet;
 use std::io::Write;
 use std::path::PathBuf;
@@ -9,55 +6,70 @@ use std::os::unix::ffi::OsStrExt;
 
 
 
-#[derive(StructOpt)]
-#[structopt(about="Recursively get images")]
-struct CLIArguments {
-    #[structopt(short="a", long="all", help=concat!(
+#[derive(Parser)]
+#[structopt(about="Recursively get images (or non-images)")]
+pub struct Arguments {
+    #[arg(short, long,
+        action=clap::ArgAction::Count, help=concat!(
+        "Increase verbosity"))]
+    verbose: u8,
+    #[arg(short, long,
+        action=clap::ArgAction::Count, help=concat!(
+        "Decrease verbosity"))]
+    quiet: u8,
+
+    #[arg(short='a', long="all", help=concat!(
         "Enable processing of hidden subfiles/directories of targets"))]
-    dohidden: bool,
+    pub dohidden: bool,
 
-    #[structopt(short="n", long, help=concat!(
+    #[arg(short='n', long, help=concat!(
         "Disable sorting by last modified time"))]
-    no_sort: bool,
+    pub no_sort: bool,
 
-    #[structopt(short="0", long, help=concat!(
+    #[arg(short='0', long, help=concat!(
         "Use null as separator, not newline"))]
-    null: bool,
+    pub null: bool,
 
-    #[structopt(long, help=concat!(
+    #[arg(long, help=concat!(
         "Escape paths"))]
-    quote: bool,
+    pub quote: bool,
 
-    #[structopt(short, long, value_name="EXT",
-        default_value="dpx exr gif heic jpeg jpg png svg tiff webp", help=concat!(
+    #[arg(short, long, value_name="EXT",
+        value_parser, value_delimiter=',',
+        default_value="dpx,exr,gif,heic,jpeg,jpg,png,svg,tiff,webp", help=concat!(
         "File extensions to filter for"))]
-    extensions: Vec<String>,
+    pub extensions: Vec<String>,
 
-    #[structopt(value_name="TARGET", help=concat!(
-        "Target files and directories (recursive)\n",
-        "  If none specified, current working directory is implied"))]
-    targets: Vec<String>,
+    #[arg(short, long="output",
+        default_value="-", help=concat!(
+        "Output file ('-' is stdout)"))]
+    pub output_file: String,
+
+    #[arg(value_name="TARGET",
+        default_value=".", help=concat!(
+        "Target files and directories (recursive)"))]
+    pub targets: Vec<PathBuf>,
+}
+impl Arguments {
+    pub fn verbosity(&self) -> i16 {
+        self.verbose as i16 - self.quiet as i16
+    }
 }
 
 
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let args = {
-        let mut args = CLIArguments::from_args();
-        if args.targets.is_empty() {
-            args.targets.push(".".to_string());
-        }
-        args
-    };
+pub fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let args = Arguments::parse();
 
-    let valid_extensions: HashSet<&str> = args.extensions.iter().map(|s| s.as_str()).collect();
+    let verbosity = args.verbosity();
 
-    let mut registry = Registry::new(valid_extensions);
+    let valid_extensions: HashSet<&str> =
+        args.extensions.iter().map(|s| s.as_str()).collect();
+
+    let mut registry = Registry::new(verbosity, valid_extensions);
 
     registry.populate(
-        args.targets.into_iter().map(
-            |target| PathBuf::from(&target)
-        ),
+        args.targets.into_iter(),
         args.dohidden
     );
 
@@ -65,21 +77,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         registry.sort_by_modified();
     }
 
-    let mut stdout_buffer = std::io::BufWriter::new(std::io::stdout().lock());
-    registry.write_all(&mut stdout_buffer, args.null, args.quote)?;
+    match args.output_file.as_ref() {
+        "-" => registry.write_all(
+            &mut std::io::BufWriter::new(
+                std::io::stdout().lock()
+            ),
+            args.null, args.quote
+        )?,
+        file => registry.write_all(
+            &mut std::io::BufWriter::new(
+                std::fs::File::create(file).map_err(
+                    |e| format!("Failed to write to output file: {}", e))?
+            ),
+            args.null, args.quote
+        )?,
+    };
 
     Ok(())
 }
 
 
 
-struct Registry<'a> {
+pub struct Registry<'a> {
+    verbosity: i16,
     registry: Vec<(std::fs::Metadata, PathBuf)>,
     valid_extensions: HashSet<&'a str>
 }
 impl<'a> Registry<'a> {
-    pub fn new(valid_extensions: HashSet<&'a str>) -> Self {
-        Self { registry: Vec::new(), valid_extensions }
+    pub fn new(verbosity: i16, valid_extensions: HashSet<&'a str>) -> Self {
+        Self {
+            verbosity,
+            registry: Default::default(),
+            valid_extensions
+        }
     }
 
     pub fn write_all(&self, writer: &mut impl Write, separator_null: bool, quote: bool) -> std::io::Result<()> {
@@ -95,8 +125,8 @@ impl<'a> Registry<'a> {
     }
 
     pub fn sort_by_modified(&mut self) {
-        self.registry.sort_by_key(|(meta,_)| {
-            meta.modified().ok().unwrap_or_else(
+        self.registry.sort_by_key(|(md,_)| {
+            md.modified().ok().unwrap_or_else(
                 || std::time::SystemTime::UNIX_EPOCH,
             )
         });
@@ -114,39 +144,73 @@ impl<'a> Registry<'a> {
         }
     }
 
-    fn add_file(&mut self, path: PathBuf, metadata: std::fs::Metadata) {
+    pub fn add_file(&mut self, path: PathBuf, metadata: std::fs::Metadata) {
         if let Some(osstr_ext) = path.extension() {
             match osstr_ext.to_str() {
                 Some(ext) => if self.valid_extensions.contains(ext) {
                     self.registry.push((metadata, path));
                 },
-                None => eprintln!(
-                    "Cannot read non-utf-8 file extension: {} on {}",
-                    shlex::try_quote(&osstr_ext.to_string_lossy()).unwrap(),
-                    shlex::try_quote(&path.to_string_lossy()).unwrap(),
-                )
+                None => match self.verbosity {
+                    ..0 => (),
+                    0.. => eprintln!(
+                        "Cannot read non-utf-8 file extension: {} on {}",
+                        shlex::try_quote(&osstr_ext.to_string_lossy()).unwrap(),
+                        shlex::try_quote(&path.to_string_lossy()).unwrap(),
+                    ),
+                },
             }
         }
     }
 
-    fn add_dir(&mut self, path: PathBuf, dohidden: bool) {
-        if let Ok(entries) = std::fs::read_dir(path) {
-            for path in entries.filter_map(|e| e.ok() ).map(|e| e.path() ) {
-                // unwrap below is safe because it is only None if the file_name is .. or is root /
-                //   (neither of which would happen in this scenario)
-                if !dohidden && path.file_name().map(|name| name.to_string_lossy().starts_with('.')).unwrap_or(true) {
-                    continue
+    pub fn add_dir(&mut self, path: PathBuf, dohidden: bool) {
+        let entries = match std::fs::read_dir(path) {
+            Ok(entries) => entries,
+            Err(e) => {
+                match self.verbosity {
+                    ..0 => (),
+                    0.. => eprintln!("{}", e),
                 }
-                if let Ok(metadata) = std::fs::symlink_metadata(&path) {
-                    if metadata.file_type().is_symlink() {
-                        continue
+                return;
+            },
+        };
+
+        for e in entries {
+            let path = match e {
+                Ok(e) => e.path(),
+                Err(e) => {
+                    match self.verbosity {
+                        ..0 => (),
+                        0.. => eprintln!("{}", e),
                     }
-                    if path.is_file() {
-                        self.add_file(path, metadata)
-                    } else if path.is_dir() {
-                        self.add_dir(path, dohidden);
+                    continue;
+                },
+            };
+
+            // unwrap is fine because it is only None if the file_name is .. or is root /
+            //   (neither of which would happen in this scenario)
+            if !dohidden && path.file_name().map(|name| name.to_string_lossy().starts_with('.')).unwrap_or(true) {
+                continue
+            }
+
+            let metadata = match std::fs::symlink_metadata(&path) {
+                Ok(md) => md,
+                Err(e) => {
+                    match self.verbosity {
+                        ..0 => (),
+                        0.. => eprintln!("{}", e),
                     }
+                    continue;
                 }
+            };
+
+            if metadata.file_type().is_symlink() {
+                continue
+            }
+
+            if path.is_file() {
+                self.add_file(path, metadata);
+            } else if path.is_dir() {
+                self.add_dir(path, dohidden);
             }
         }
     }
